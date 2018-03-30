e1083083527ccf26654dfa92fa6584f7f6fc9071
Zsolnai Csaba <kanesoban@gmail.com>
2018-03-30 23:56:07 +0200

    deep reinforcement learning

--- models.py
+++ models.py
@@ -3,20 +3,20 @@ import tensorflow as tf
 
 
 class SimpleGD:
-    def __init__(self, session, loss, model_parameters, parameter, label, learning_rate=0.01):
+    def __init__(self, session, loss, variables, learning_rate=0.01):
         self.learning_rate = learning_rate
         self.session = session
         self.loss = loss
-        self.w = model_parameters[0]
-        self.b = model_parameters[1]
-        self.parameter = parameter
-        self.label = label
+        self.w = variables['w']
+        self.b = variables['b']
+        self.x = variables['x']
+        self.y = variables['y']
         self.gradients = tf.gradients(loss, [self.w, self.b])
 
     def partial_fit(self, x, y):
         for model_parameter, gradient in zip([self.w, self.b], self.gradients):
             new_model_parameter = self.session.run((model_parameter - self.learning_rate * gradient),
-                                                    feed_dict={self.parameter: x, self.label: y})
+                                                   feed_dict={self.x: x, self.y: y})
             self.session.run(model_parameter.assign(new_model_parameter))
 
 
@@ -27,21 +27,20 @@ class SimpleSGDRegressor():
         with tf.variable_scope('SimpleSGDRegressor' + scope_id, reuse=tf.AUTO_REUSE):
             self.w = tf.get_variable('w', shape=(1, dimensions), initializer=tf.zeros_initializer(), dtype=tf.float32)
             self.b = tf.get_variable('b', shape=(1,), initializer=tf.zeros_initializer(), dtype=tf.float32)
-            y = tf.get_variable('y', shape=(1,), initializer=tf.zeros_initializer(), dtype=tf.float32)
-            parameter = tf.placeholder(shape=(1, dimensions), dtype=tf.float32)
+            x = tf.placeholder(shape=(1, dimensions), dtype=tf.float32)
+            y = tf.placeholder('y', shape=(1,), initializer=tf.zeros_initializer(), dtype=tf.float32)
+            variables = {'w': self.w, 'b': self.b, 'x': x, 'y': y}
         init = tf.global_variables_initializer()
         self.session.run(init)
-        model_parameters = [self.w, self.b]
-        loss = (y - tf.matmul(self.w, tf.transpose(parameter)) - self.b) ** 2
-        self.gradient_descent = SimpleGD(self.session, loss, model_parameters, parameter, y, self.learning_rate)
+        loss = (y - tf.matmul(self.w, tf.transpose(x)) - self.b) ** 2
+        self.gradient_descent = SimpleGD(self.session, loss, variables, self.learning_rate)
 
     def partial_fit(self, x, y):
         self.gradient_descent.partial_fit(x, y)
 
     def fit(self, X, Y):
-        for _ in range(self.max_iter):
-            for x, y in zip(X, Y):
-                self.partial_fit(x.reshape((1, -1)), y)
+        for x, y in zip(X, Y):
+            self.partial_fit(x.reshape((1, -1)), y)
 
     def predict(self, x):
         return self.session.run(tf.matmul(self.w, tf.transpose(x)) + self.b).ravel()
