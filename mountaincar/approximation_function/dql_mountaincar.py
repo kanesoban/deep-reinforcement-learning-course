75a0e88cf5e88abe5140d49ee9b21ec2558edd85
Zsolnai Csaba <kanesoban@gmail.com>
2018-04-08 18:21:25 +0200

    Fixes for deep q-learning mountaincar

--- mountaincar/approximation_function/dql_mountaincar.py
+++ mountaincar/approximation_function/dql_mountaincar.py
@@ -8,14 +8,13 @@ from visualization import plot_cost_to_go
 from visualization import plot_running_avg
 
 
-def play_one_episode(environment, epsilon, gamma=0.99, max_steps=10000):
+def play_one_episode(session, environment, epsilon, gamma=0.99, max_steps=10000):
     observation = environment.reset()
     done = False
     time_step = 0
     total_reward = 0
-    session = tf.Session()
-    model = MountainCarNeuralNetwork(session, 4, 2)
-
+    model = MountainCarNeuralNetwork(session, 2, 2, environment)
+    session.run(tf.global_variables_initializer())
     while not done and time_step < max_steps:
         time_step += 1
         # Choose E-greedy action
@@ -27,9 +26,10 @@ def play_one_episode(environment, epsilon, gamma=0.99, max_steps=10000):
         if done:
             reward = 100
         # Update
-        next_action_predictions = model.predict(next_observation)
+        next_action_predictions = model.predict(next_observation.reshape((1, -1)))
         state_action_value = reward + gamma * numpy.max(next_action_predictions[0])
-        model.update(observation, state_action_value)
+        model.update(observation.reshape((1, -1)),
+                     numpy.array([state_action_value]).astype(dtype=numpy.float32))
         if done:
             break
         observation = next_observation
@@ -37,12 +37,13 @@ def play_one_episode(environment, epsilon, gamma=0.99, max_steps=10000):
 
 
 def play_multiple_episodes(environment, episodes):
-    total_rewards = numpy.empty(episodes)
-    for i in progress_bar(range(episodes), desc='Playing episode'):
-        epsilon = 1.0 / numpy.sqrt(1 + i)
-        total_rewards[i] = play_one_episode(environment, epsilon)
-    plot_running_avg(total_rewards)
-    plot_cost_to_go(environment.observation_space)
+    with tf.Session() as session:
+        total_rewards = numpy.empty(episodes)
+        for i in progress_bar(range(episodes), desc='Playing episode'):
+            epsilon = 1.0 / numpy.sqrt(1 + i)
+            total_rewards[i] = play_one_episode(session, environment, epsilon)
+        plot_running_avg(total_rewards)
+        plot_cost_to_go(environment.observation_space)
 
 
 if __name__ == '__main__':
